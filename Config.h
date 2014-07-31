2ffb01c0e080387d29428cce912fea244dcaff71
Wissem BOUSSETTA <boussetta.wissem@hotmail.fr>
2014-07-31 23:34:01 +0200

    Hello SystemC project created. Via several samples, this project gives a good overview on mixed system development using systemC library with eclipse IDE.
    First commit:
    project creation.
    1. config file.
    2. RSA sample.

---
+++ Config.h
@@ -0,0 +1,15 @@
+/*
+ * Config.h
+ *
+ *  Created on: 31 Jul 2014
+ *      Author: Wissem
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <systemc.h>
+
+#define SYSTEM_C_RSA_SAMPLE	1
+
+#endif /* CONFIG_H_ */
